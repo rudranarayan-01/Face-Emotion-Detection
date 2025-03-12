d4fef35611f59c823c534e681082bc359a47556c
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2025-03-12 23:33:39 +0530

    a

--- detect.py
+++ detect.py
@@ -16,7 +16,7 @@ def extract_features(image):
     feature = feature.reshape(1,48,48,1)
     return feature/255.0
 
-webcam=cv2.VideoCapture(0)
+webcam=cv2.VideoCapture(0) 
 labels = {0 : 'angry', 1 : 'disgust', 2 : 'fear', 3 : 'happy', 4 : 'neutral', 5 : 'sad', 6 : 'surprise'}
 while True:
     i,im=webcam.read()
